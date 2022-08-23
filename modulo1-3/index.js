fac5890ee5cb72c480edd732b79e6f028ed13dc5
Alexc0217 <alex.costa.moura@outlook.com>
2022-08-23 11:26:27 -0300

    Revert "unit test project"
    This reverts commit 18a90d6ce27cbbc71093b27236f2cd6816b8295a.

--- modulo1-3/index.js
+++
@@ -1,6 +0,0 @@
-const somaHoraExtras = (salario, valorHorasExtras) =>
-  salario + valorHorasExtras;
-
-const calculaDescontos = (salario, descontos) => salario - descontos;
-
-export { somaHoraExtras, calculaDescontos }
